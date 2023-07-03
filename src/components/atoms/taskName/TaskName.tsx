82b1199d31df1fa92c0123b650794febd293d729
izzana <izzana.martins@gmail.com>
2023-07-03 01:32:02 -0300

    feat: new features like filters, checks

--- src/components/atoms/taskName/TaskName.tsx
+++ src/components/atoms/taskName/TaskName.tsx
@@ -1,33 +1,48 @@
-import React, {FC, useState} from "react";
-import {ITaskName} from "../../../types/components/atoms/taskName";
+import React, { FC, useState, useEffect } from "react";
+import { ITaskName } from "../../../types/components/atoms/taskName";
+import { useFormsTask } from "../../../hooks/FormTask";
+
 import Checkbox from "../checkBox/CheckBox";
 import '../../../index.css';
-import './TaskName.css'
+import './TaskName.css';
 
 const TaskName: FC<ITaskName> = (params: ITaskName) => {
+  const {
+    checked,
+    strikeThrough,
+    task,
+    setIsClosed,
+    isClosed,
+    setChecked,
+    setStrikeThrough,
+  } = useFormsTask();
+
   const taskName = params.taskName;
   const taskDescription = params.taskDescription;
-
-  const [checked, setChecked] = useState<boolean>(false);
-  const [strikeThrough, setStrikeThrough] = useState<boolean>(false);
-
-  const handleCheckBox = () => {
+  const id = params.id;
+  
+  const handleCheckBox = (id: any) => {
     setChecked(!checked);
     setStrikeThrough(!strikeThrough);
-  } 
+    setIsClosed(!checked);
+    console.log("id: " + id)
+  };
+
 
   return (
     <div className="flex-row justify-content-between align-center task-container">
       <div className="flex-column justify-content-between">
-      <h2 className={strikeThrough ? "strike" : ""}>{taskName}</h2>
+        <h2 className={strikeThrough ? "strike" : ""}>{taskName}</h2>
         <p className="task-bottom">{taskDescription}</p>
       </div>
-      <Checkbox 
+      <Checkbox
         isChecked={checked}
-        onChange={handleCheckBox}
+        onChange={() => handleCheckBox(id)}
       />
     </div>
-  )
-}
+
+  );
+  
+};
 
 export default TaskName;
